f5799619bdf9667128587127b77e760f302798da
itggot-edvin-alestig <edvin.alestig@itggot.se>
2018-10-05 17:21:29 +0200

    Added 2 location and a check_response function

--- vasttrafik.py
+++ vasttrafik.py
@@ -12,17 +12,17 @@ class Vasttrafik():
         if type(scope) != int:
             raise TypeError("Expected int [scope]")
 
-        self.credentials = base64.b64encode(str.encode(f'{key}:{secret}')).decode("utf-8")
+        self.__credentials = base64.b64encode(str.encode(f'{key}:{secret}')).decode("utf-8")
         self.scope = scope
 
         self.__renew_token()
 
 
     def __renew_token(self):
-        print(self.credentials)
+        print(self.__credentials)
         header = {
             "Content-Type": "application/x-www-form-urlencoded",
-            "Authorization": "Basic " + self.credentials
+            "Authorization": "Basic " + self.__credentials
         }
         url = f'https://api.vasttrafik.se/token?grant_type=client_credentials&scope=device_{self.scope}'
         response = requests.post(url, headers=header)
@@ -36,24 +36,55 @@ class Vasttrafik():
         self.token = "Bearer " + response_dict.get("access_token")
 
 
-    def trip(self, **kwargs):
-        header = {"Authorization": self.token}
-        url = "https://api.vasttrafik.se/bin/rest.exe/v2/trip"
-        kwargs["format"] = "json"
-        response = requests.get(url, headers=header, params=kwargs)
-
-        print(response.url)
-        print(response.status_code)
-        print(response.text)
+    def __check_response(self, response):
         if response.status_code == 401:
             self.__renew_token()
-            response = requests.get(url, headers=header, params=kwargs)
+
+            header = {"Authorization": self.token}
+            response = requests.get(response.url, headers=header)
 
         response_dict = response.json()
         if response.status_code != 200:
             raise requests.exceptions.HTTPError(f'{response.status_code} {response_dict.get("error_description")}')
 
-        return response_dict
+        return response
+
+
+    def trip(self, **kwargs):
+        header = {"Authorization": self.token}
+        url = "https://api.vasttrafik.se/bin/rest.exe/v2/trip"
+        kwargs["format"] = "json"
+
+        response = requests.get(url, headers=header, params=kwargs)
+        response = self.__check_response(response)
+
+        return response.json()
+
+
+    def location_nearbyaddress(self, **kwargs):
+        header = {"Authorization": self.token}
+        url = "https://api.vasttrafik.se/bin/rest.exe/v2/location.nearbyaddress"
+        kwargs["format"] = "json"
+ 
+        response = requests.get(url, headers=header, params=kwargs)
+        response = self.__check_response(response)
+
+        return response.json()
+
+
+    def location_nearbystops(self, **kwargs):
+        header = {"Authorization": self.token}
+        url = "https://api.vasttrafik.se/bin/rest.exe/v2/location.nearbystops"
+        kwargs["format"] = "json"
+
+        response = requests.get(url, headers=header, params=kwargs)
+        response = self.__check_response(response)
+
+        return response.json()
+
+
+    
+
 
 
 
